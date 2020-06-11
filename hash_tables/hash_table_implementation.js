743dfc499b59299b7fa7ed07309604e41b00aec4
Taha Baz <etahabaz@gmail.com>
2020-06-11 23:21:54 +0100

    Fixe bug in the set method

--- hash_tables/hash_table_implementation.js
+++ hash_tables/hash_table_implementation.js
@@ -12,22 +12,26 @@ const HashTableFactory = function (size) {
             data: new Array(size),
             set: function (key, value) {
                 const adress = hash.call(this, key)
-                if (!Array.isArray(this.data[adress]))
+                if (!Array.isArray(this.data[adress])) {
                     this.data[adress] = [key, value]
-                else
-                {
+                }
+                else if (!Array.isArray(this.data[adress][0])){
                     const arr = []
-                    arr.push(this.data[adress], [key, value])
+                    arr.push(this.data[adress])
+                    arr.push([key, value])
                     this.data[adress] = arr
+                }
+                else {
+                    this.data[adress].push([key, value])
                 }
             },
             get: function (key) {
                 const adress = hash.call(this, key)
                 const currentBucket = this.data[adress]
-                if (Array.isArray(currentBucket)){
+                if (Array.isArray(currentBucket)) {
                     if (Array.isArray(currentBucket[0])) {
                         let i = 0
-                        for (let element of currentBucket){
+                        for (let element of currentBucket) {
                             if (element[0] === key)
                                 return element[1]
                             i++;
@@ -37,11 +41,11 @@ const HashTableFactory = function (size) {
                 }
                 return undefined
             },
-            keys: function() {
+            keys: function () {
                 const keys = []
-                for(let element of this.data){
+                for (let element of this.data) {
                     if (Array.isArray(element)) {
-                        if (Array.isArray(element[0])){
+                        if (Array.isArray(element[0])) {
                             element.forEach(el => {
                                 keys.push(el[0])
                             })
@@ -57,10 +61,14 @@ const HashTableFactory = function (size) {
 }
 
 const HashTable = HashTableFactory()
-const hashObj = HashTable(20)
+const hashObj = HashTable(10)
 
 hashObj.set('taha', '22')
 hashObj.set('baz', '244')
+hashObj.set('med', '244')
+hashObj.set('tt', '244')
+
 
 
 console.log(hashObj.keys());
+
