5cf562b0329d9b7a99fb4f2e99dc67583757fb37
Taha Baz <etahabaz@gmail.com>
2020-06-11 16:49:43 +0100

    exploit the closure behavior to make the hash function private

--- hash_tables/hash_table_implementation.js
+++ hash_tables/hash_table_implementation.js
@@ -1,34 +1,36 @@
 
-const _hash = function (key) {
-    let hash = 0
-    for (let i = 0; i < key.length; i++) {
-        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+const HashTableFactory = function (size) {
+    const hash = function (key) {
+        let hash = 0
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash
     }
-    return hash
-}
-
-const makeHashTable = function (size) {
-    return {
-        data: new Array(size),
-        set: function (key, value) {
-            this.data[_hash.call(this, key)] = [key, value]
-        },
-        get: function (key) {
-            if (Array.isArray(this.data[_hash.call(this, key)]))
-                return this.data[_hash.call(this, key)][1]
-            return undefined
+    return function (size) {
+        return {
+            data: new Array(size),
+            set: function (key, value) {
+                this.data[hash.call(this, key)] = [key, value]
+            },
+            get: function (key) {
+                if (Array.isArray(this.data[hash.call(this, key)]))
+                    return this.data[hash.call(this, key)][1]
+                return undefined
+            }
         }
     }
 }
 
-const hashObj = makeHashTable(30)
+const HashTable = HashTableFactory()
+const hashObj = HashTable(30)
 
 hashObj.set('taha', '22')
 hashObj.set('baz', '244')
 hashObj.set('mohamed', '2')
 hashObj.set('hmida', '0')
 
-console.log(hashObj.get('taha'))
-console.log(hashObj.get('baz'))
-console.log(hashObj.get('hmida'))
-console.log(hashObj.get('mohamed'))
+console.log((hashObj.get('taha')))
+console.log((hashObj.get('baz')))
+console.log((hashObj.get('mohamed')))
+console.log((hashObj.get('hmida')))
