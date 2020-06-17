64b1db784425e7da008baa35b5bbc104f0c22916
Taha Baz <etahabaz@gmail.com>
2020-06-17 23:09:59 +0100

    remove implementation with a spaguetti code

--- trees/binary_search_tree_implementation.js
+++ trees/binary_search_tree_implementation.js
@@ -38,7 +38,7 @@ const tree = {
     },
     lookup(value) {
         let tempNode = this.root
-        while (tempNode !== null){
+        while (tempNode !== null) {
             if (value === tempNode.value)
                 return tempNode
             else if (value < tempNode.value)
@@ -47,6 +47,79 @@ const tree = {
                 tempNode = tempNode.right
         }
         return false
+    },
+    lookupToRemove(value) {
+        if (this.root === null) {
+            return null
+        } else if (this.root.value === value) {
+            return this.root
+        }
+        else {
+            let nodeToRemove = this.root
+            while (true) {
+                if (value <= nodeToRemove.left.value) {
+                    if (nodeToRemove.left.value === value) {
+                        return {
+                            nodeToRemove: nodeToRemove.left,
+                            parentNode: {
+                                node: nodeToRemove,
+                                dir: 'left'
+                            }
+                        }
+                    } else
+                        nodeToRemove = nodeToRemove.left
+                }
+                else if (value >= nodeToRemove.right.value) {
+                    if (nodeToRemove.right.value === value) {
+                        return {
+                            nodeToRemove: nodeToRemove.right,
+                            parentNode: {
+                                node : nodeToRemove,
+                                dir: 'right'
+                            }
+                        }
+                    } else
+                        nodeToRemove = nodeToRemove.right
+                }
+            }
+        }
+    },
+    remove(value) {
+        let node = this.lookupToRemove(value)
+        let nodeToRemove = node.nodeToRemove
+        let parentNode = node.parentNode
+        let childParentNode = {
+            dir: 0
+        }
+        while (true) {
+            if (nodeToRemove.right === null) {
+                if (nodeToRemove.left === null) {
+                    if (childParentNode.dir !== 0) {
+                        let parrentdir = parentNode.dir
+                        let temp = parentNode.node[parentNode.dir]
+                        parentNode.node[parrentdir] = nodeToRemove
+                        nodeToRemove.left = childParentNode.nodeToRemove[childParentNode.dir]
+                        childParentNode.nodeToRemove[childParentNode.dir] = null
+                    }
+                    else
+                        parentNode.node[parentNode.dir] = null
+                    return true
+                }
+                else {
+                    childParentNode = {
+                        nodeToRemove,
+                        dir: 'right'
+                    }
+                    nodeToRemove = nodeToRemove.left
+                }
+            } else {
+                childParentNode = {
+                    nodeToRemove,
+                    dir: 'left'
+                }
+                nodeToRemove = nodeToRemove.right
+            }
+        }
     }
 }
 
@@ -54,9 +127,16 @@ const tree = {
 const mytree = Object.create(tree)
 mytree.init()
 mytree.insert(9)
+mytree.insert(4)
+mytree.insert(20)
+mytree.insert(1)
+mytree.insert(6)
+mytree.insert(15)
+mytree.insert(170)
 
-console.log(mytree.lookup(9))
-//console.log(JSON.stringify(traverse(mytree.root)))
+const a = mytree.remove(20)
+
+console.log(JSON.stringify(traverse(mytree.root)))
 
 //     9
 //  4     20
